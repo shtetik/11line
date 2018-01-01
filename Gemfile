25233e4d41b6094a7ed84fc076078606eae245d1
Pavel Kulikov <shtetik@gmail.com>
2018-01-01 12:20:10 +0530

    Inline SVG helper. Footer.

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'middleman-sprockets', '~> 4.1', '>= 4.1.1'
 
 gem 'therubyracer', '~> 0.12.3'
 gem 'oj', '~> 3.3', '>= 3.3.9'
+gem 'oga', '~> 2.12'
 
 gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
 gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw]
---
+++ helpers/application_helpers.rb
@@ -0,0 +1,32 @@
+module ApplicationHelpers
+  def inline_svg(filename, options = {})
+    root = Middleman::Application.root
+    images_path = config[:images_dir]
+    file_path = "#{root}/source/#{images_path}/#{filename}"
+
+    if File.exists?(file_path)
+      file = File.open(file_path, 'r') { |f| f.read }
+      doc = Oga.parse_xml(file)
+
+      svg = doc.css('svg').first
+
+      if options[:class].present?
+        svg.set(:class, options[:class])
+      end
+
+      doc.to_xml
+    else
+      "<!-- SVG file not found: #{not_found_message(filename)} -->".html_safe
+    end
+  end
+
+  private
+
+  def not_found_message(filename)
+    "'#{filename}' #{extension_hint(filename)}"
+  end
+
+  def extension_hint(filename)
+    filename.end_with?(".svg") ? "" : "(Try adding .svg to your filename)"
+  end
+end
